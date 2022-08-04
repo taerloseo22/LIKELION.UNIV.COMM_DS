ffb876de969e5e0d632d8f613335eaa749ee05cf
namseohyeon <syn5421a@gmail.com>
2022-08-04 16:11:34 +0900

    CR구현(create,read)+사진, 파일 업로드 기능

--- commapp/models.py
+++ commapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class comm(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    img = models.ImageField(blank=True, null=True, upload_to='lion_photo/%y/%m/%d/')
+    file = models.FileField(blank=True, null=True,upload_to='lion_file/%y/%m/%d/')
+
+    def __str__(self):
+        return self.title
+
