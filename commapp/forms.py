1f6552ee25057a5fcad48ddae9d113c9ea6cb90f
DAASHeo <hibstar719@naver.com>
2022-08-16 21:39:59 +0900

    디테일 페이지 1차 초안

--- commapp/forms.py
+++ commapp/forms.py
@@ -18,6 +18,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content', )
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': "commentform",
+                'placeholder': '댓글을 작성하세요'
+                }),
+        }
 
 
 class ReCommentForm(forms.ModelForm):
