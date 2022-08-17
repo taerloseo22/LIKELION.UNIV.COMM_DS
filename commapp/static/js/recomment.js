495cc5b28c3118ce1de118c9514603fa431d50b5
DAASHeo <hibstar719@naver.com>
2022-08-17 19:35:02 +0900

    댓글 수정, 글 미리보기 글 줄임

--- commapp/static/js/recomment.js
+++ commapp/static/js/recomment.js
@@ -1,6 +1,12 @@
 var btn = document.querySelector(".rebtn")
 var section = document.querySelector(".resection")
 function recomment(){
+    if(btn.style.display==="flex"){
     section.style.display="block";
     btn.style.display="none";
+    }
+    else{
+        section.style.display="none";
+        btn.style.display="flex"; 
+    }
 }
